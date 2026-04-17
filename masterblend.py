7692450699e1a96e630636549038af59d6fbf9ff
Jeferson <55026607+jeferson721@users.noreply.github.com>
2026-04-17 12:46:06 -0300

    Adicionar estrutura inicial do complemento MasterBlend
    Implementação inicial do complemento MasterBlend para Blender, incluindo metadados e funções de registro.

---
+++ masterblend.py
@@ -0,0 +1,21 @@
+bl_info = {
+    "name": "MasterBlend",
+    "author": "Jeferson Martins",
+    "version": (1, 0),
+    "blender": (5, 0, 0),
+    "location": "View3D > Sidebar > Master Blend",
+    "description": "Kit de ferramentas voltadas para a arquitetura e renderização.",
+    "category": "Object",
+}
+
+
+import bpy
+import bmesh
+import math
+
+def register():
+
+def unregister():
+
+if __name__ == "__main__":
+    register()
