bc50a3122ee6b6efd60568ae2c99e67af83d94a4
Jeferson <55026607+jeferson721@users.noreply.github.com>
2026-04-18 18:33:13 -0300

    Implementação do mecanismo de renderização ao painel Master Blend

--- masterblend.py
+++ masterblend.py
@@ -8,14 +8,57 @@ bl_info = {
     "category": "Object",
 }
 
-
 import bpy
 import bmesh
 import math
 
+
+def update_render_engine(self, context):
+    if self.render_engine == 'CYCLES':
+        context.scene.render.engine = 'CYCLES'
+    elif self.render_engine == 'BLENDER_EEVEE':
+        context.scene.render.engine = 'BLENDER_EEVEE'
+
+
+class MB_Properties(bpy.types.PropertyGroup):
+    render_engine: bpy.props.EnumProperty(
+        name="Render Engine",
+        items=[
+            ('BLENDER_EEVEE', "Eevee", ""),
+            ('CYCLES', "Cycles", "")
+        ],
+        update=update_render_engine
+    )
+
+
+class menu_principal(bpy.types.Panel):
+    bl_label = "Master Blend"
+    bl_idname = "MENU_PT_master_blend"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = "Master Blend"
+
+    def draw(self, context):
+        layout = self.layout
+        wm = context.window_manager
+
+        layout.label(text="Master Blend", icon='COLORSET_02_VEC')
+        layout.prop(wm.mb_props, "render_engine", expand=True)
+
+
+
+classes = (MB_Properties, menu_principal)
+
 def register():
+    for cls in classes:
+        bpy.utils.register_class(cls)
+    bpy.types.WindowManager.mb_props = bpy.props.PointerProperty(type=MB_Properties)
 
 def unregister():
+    for cls in reversed(classes):
+        bpy.utils.unregister_class(cls)
+    del bpy.types.WindowManager.mb_props
+
 
 if __name__ == "__main__":
     register()
