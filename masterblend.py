274c6f96ecbcfa6d97f243489a7f88a1f33e9dbe
Jeferson <55026607+jeferson721@users.noreply.github.com>
2026-04-20 12:58:57 -0300

    Implementação da função com submenu: material override

--- masterblend.py
+++ masterblend.py
@@ -12,13 +12,11 @@ import bpy
 import bmesh
 import math
 
-
 def update_render_engine(self, context):
     if self.render_engine == 'CYCLES':
         context.scene.render.engine = 'CYCLES'
     elif self.render_engine == 'BLENDER_EEVEE':
         context.scene.render.engine = 'BLENDER_EEVEE'
-
 
 class MB_Properties(bpy.types.PropertyGroup):
     render_engine: bpy.props.EnumProperty(
@@ -30,6 +28,57 @@ class MB_Properties(bpy.types.PropertyGroup):
         update=update_render_engine
     )
 
+class material_override(bpy.types.Operator):
+    bl_idname = "master_blend.material_override"
+    bl_label = "Material Override"
+    
+    def execute(self, context):
+        
+        for material in bpy.data.materials:          
+            if material.use_nodes is False:
+                material.use_nodes = True
+            principled_node = material.node_tree.nodes.new(type='ShaderNodeBsdfPrincipled')
+            principled_node.name = 'principled_node_OVERRIDE'
+            principled_node.inputs['Roughness'].default_value = 0.2
+            Output_node = material.node_tree.nodes.new(type='ShaderNodeOutputMaterial')
+            Output_node.name = 'Output_node_OVERRIDE'
+            material.node_tree.links.new(principled_node.outputs['BSDF'], Output_node.inputs['Surface'])
+            
+            if material.node_tree and "Output_node_OVERRIDE" in material.node_tree.nodes:
+                node_to_active = material.node_tree.nodes["Output_node_OVERRIDE"]
+                material.node_tree.nodes.active = node_to_active
+                
+        return {'FINISHED'}
+
+class remove_override(bpy.types.Operator):
+    bl_idname = "master_blend.remove_material_override"
+    bl_label = "Material Override"
+    
+    def execute(self, context):
+                        
+        for material in bpy.data.materials:          
+                        
+            if material.node_tree and "Output_node_OVERRIDE" in material.node_tree.nodes:
+                node_to_delete = material.node_tree.nodes["Output_node_OVERRIDE"]
+                material.node_tree.nodes.remove(node_to_delete)
+                
+            if material.node_tree and "principled_node_OVERRIDE" in material.node_tree.nodes:
+                node_to_delete = material.node_tree.nodes["principled_node_OVERRIDE"]
+                material.node_tree.nodes.remove(node_to_delete)
+                
+            if material.node_tree and "Material Output" in material.node_tree.nodes:
+                    node_to_active = material.node_tree.nodes["Material Output"]
+                    material.node_tree.nodes.active = node_to_active
+                
+        return {'FINISHED'}
+
+class submenu_0(bpy.types.Menu):
+    bl_idname = "submenu_0.name"
+    bl_label = ""
+    def draw(self, context):
+        layout = self.layout             
+        layout.operator("master_blend.material_override", text='Aplicar')         
+        layout.operator("master_blend.remove_material_override", text='Remover')
 
 class menu_principal(bpy.types.Panel):
     bl_label = "Master Blend"
@@ -44,10 +93,10 @@ class menu_principal(bpy.types.Panel):
 
         layout.label(text="Master Blend", icon='COLORSET_02_VEC')
         layout.prop(wm.mb_props, "render_engine", expand=True)
+        layout.label(text="Material Override")
+        layout.menu(submenu_0.bl_idname)      
 
-
-
-classes = (MB_Properties, menu_principal)
+classes = (MB_Properties, submenu_0, remove_override, material_override, menu_principal)
 
 def register():
     for cls in classes:
@@ -58,7 +107,6 @@ def unregister():
     for cls in reversed(classes):
         bpy.utils.unregister_class(cls)
     del bpy.types.WindowManager.mb_props
-
 
 if __name__ == "__main__":
     register()
