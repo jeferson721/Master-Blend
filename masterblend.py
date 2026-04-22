f50d1a45f7b0c55280a33103334584456c940ce2
Jeferson <55026607+jeferson721@users.noreply.github.com>
2026-04-22 14:44:54 -0300

    Implementação da função master_media()

--- masterblend.py
+++ masterblend.py
@@ -143,6 +143,91 @@ class master_baixa(bpy.types.Operator):
             
         return {'FINISHED'}
 
+class master_media(bpy.types.Operator):
+    bl_idname = "master_blend.media"
+    bl_label = "Configuração engines"
+    
+    def execute(self, context):
+        if bpy.data.window_managers["WinMan"].Evee:        
+            bpy.context.scene.render.engine = 'BLENDER_EEVEE'
+            bpy.context.scene.eevee.taa_render_samples = 64
+            bpy.context.scene.eevee.taa_samples = 16
+            bpy.context.scene.eevee.use_gtao = True
+            bpy.context.scene.eevee.gtao_quality = 1
+            bpy.context.scene.eevee.use_ssr = True
+            bpy.context.scene.eevee.use_ssr_refraction = True
+            bpy.context.scene.eevee.use_shadow_high_bitdepth = True
+            bpy.context.scene.eevee.shadow_cube_size = '1024'
+            bpy.context.scene.eevee.shadow_cascade_size = '1024'
+            bpy.context.scene.eevee.gi_cubemap_resolution = '256'
+            bpy.context.scene.eevee.gi_visibility_resolution = '16'
+            bpy.context.scene.display_settings.display_device = 'sRGB'
+            bpy.context.scene.view_settings.view_transform = 'AgX'
+            bpy.context.scene.view_settings.look = 'AgX - Punchy'
+            bpy.context.scene.view_settings.exposure = 0
+            bpy.context.scene.view_settings.gamma = 1
+            bpy.context.scene.view_settings.use_curve_mapping = False
+            bpy.context.scene.sequencer_colorspace_settings.name = 'sRGB'
+            bpy.context.scene.render.use_border = False
+            bpy.context.scene.render.image_settings.file_format = 'PNG'
+            bpy.context.scene.render.image_settings.color_mode = 'RGBA'
+            bpy.context.scene.render.image_settings.color_depth = '16'
+            bpy.context.scene.render.image_settings.color_management = 'FOLLOW_SCENE'
+            bpy.context.scene.use_nodes = False            
+        if bpy.data.window_managers["WinMan"].Cycles:
+            bpy.context.scene.render.engine = 'CYCLES'
+            bpy.context.scene.cycles.use_preview_denoising = True
+            bpy.context.scene.cycles.preview_denoising_start_sample = 499
+            bpy.context.scene.cycles.preview_samples = 500
+            bpy.context.scene.cycles.preview_adaptive_threshold = 0.1
+            bpy.context.scene.cycles.preview_adaptive_min_samples = 0
+            bpy.context.scene.cycles.adaptive_threshold = 0.01
+            bpy.context.scene.cycles.samples = 2000
+            bpy.context.scene.cycles.adaptive_min_samples = 0
+            bpy.context.scene.cycles.use_denoising = False
+            bpy.context.scene.render.use_persistent_data = True
+            bpy.context.scene.display_settings.display_device = 'sRGB'
+            bpy.context.scene.view_settings.view_transform = 'AgX'
+            bpy.context.scene.view_settings.look = 'AgX - Punchy'
+            bpy.context.scene.view_settings.exposure = 0
+            bpy.context.scene.view_settings.gamma = 1
+            bpy.context.scene.sequencer_colorspace_settings.name = 'sRGB'
+            bpy.context.scene.view_settings.use_curve_mapping = False
+            bpy.context.scene.cycles.max_bounces = 12
+            bpy.context.scene.cycles.diffuse_bounces = 4
+            bpy.context.scene.cycles.glossy_bounces = 4
+            bpy.context.scene.cycles.transmission_bounces = 12
+            bpy.context.scene.cycles.volume_bounces = 0
+            bpy.context.scene.cycles.transparent_max_bounces = 8
+            bpy.context.scene.cycles.sample_clamp_direct = 0
+            bpy.context.scene.cycles.sample_clamp_indirect = 10
+            bpy.context.scene.cycles.blur_glossy = 1
+            bpy.context.scene.cycles.caustics_reflective = True
+            bpy.context.scene.cycles.caustics_refractive = True
+            bpy.context.scene.cycles.use_fast_gi = False
+            bpy.context.scene.render.image_settings.file_format = 'PNG'
+            bpy.context.scene.render.image_settings.color_depth = '16'
+            bpy.context.scene.render.image_settings.color_management = 'FOLLOW_SCENE'
+            bpy.context.scene.render.use_border = False
+            bpy.context.view_layer.cycles.denoising_store_passes = True
+            bpy.context.scene.use_nodes = True        
+            bpy.context.scene.node_tree.nodes.clear()       
+            node_layers = bpy.context.scene.node_tree.nodes.new('CompositorNodeRLayers')
+            node_layers.name = 'node_layers_mb'
+            node_composite = bpy.context.scene.node_tree.nodes.new('CompositorNodeComposite')
+            node_composite.name = 'composite_mb'
+            node_denoiser = bpy.context.scene.node_tree.nodes.new('CompositorNodeDenoise')
+            node_denoiser.name = 'denoiser_mb'
+            tree = bpy.context.scene.node_tree 
+            links = tree.links 
+            link = links.new(node_layers.outputs["Image"], node_denoiser.inputs["Image"])
+            link = links.new(node_denoiser.outputs["Image"], node_composite.inputs["Image"])
+            link = links.new(node_layers.outputs["Denoising Normal"], node_denoiser.inputs["Normal"])
+            link = links.new(node_layers.outputs["Denoising Albedo"], node_denoiser.inputs["Albedo"])
+
+        
+        return {'FINISHED'}
+
 class submenu_0(bpy.types.Menu):
     bl_idname = "submenu_0.name"
     bl_label = ""
